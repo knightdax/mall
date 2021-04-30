3c1792a384a996268ca87bb6742074e0d8f4b25b
huangdaxin <huangdaxin@bigo.sg>
2021-04-30 15:39:02 +0800

    添加redis

---
+++ src/main/java/com/macro/mall/tiny/common/IErrorCode.java
@@ -0,0 +1,7 @@
+package com.macro.mall.tiny.common;
+
+public interface IErrorCode {
+    long getCode();
+
+    String getMessage();
+}
--- src/main/java/com/macro/mall/tiny/service/impl/PmsBrandServiceImpl.java
+++ src/main/java/com/macro/mall/tiny/service/impl/PmsBrandServiceImpl.java
@@ -3,6 +3,7 @@ package com.macro.mall.tiny.service.impl;
 import com.macro.mall.tiny.mapper.PmsBrandMapper;
 import com.macro.mall.tiny.model.PmsBrand;
 import com.macro.mall.tiny.service.PmsBrandService;
+import com.macro.mall.tiny.service.RedisService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,8 +15,13 @@ public class PmsBrandServiceImpl implements PmsBrandService {
     @Autowired
     public PmsBrandMapper pmsBrandMapper;
 
+    @Autowired
+    public RedisService redisService;
+
     @Override
     public List<PmsBrand> listAll() {
-        return pmsBrandMapper.selectByExample(null);
+        List<PmsBrand> list = pmsBrandMapper.selectByExample(null);
+        redisService.set("testValue" , list.toString());
+        return list;
     }
 }
